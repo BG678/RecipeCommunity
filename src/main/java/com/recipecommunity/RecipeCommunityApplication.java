eaa3e6f5ff6f03815481d5d341f13db27ff9a806
BG678 <57709951+BG678@users.noreply.github.com>
2020-11-29 19:03:43 +0100

    Added Maven Spring Boot project

---
+++ src/main/java/com/recipecommunity/RecipeCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.recipecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipecommunity/RecipeCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
