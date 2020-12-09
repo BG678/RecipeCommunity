31d0e39014331606ad7ad0edae298d29fde5dd9b
BG678 <57709951+BG678@users.noreply.github.com>
2020-12-09 18:48:46 +0100

    Created User repository

---
+++ src/main/java/com/recipecommunity/features/user/UserRepository.java
@@ -0,0 +1,19 @@
+package com.recipecommunity.features.user;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * User Repository interface, extends Paging and Sorting Repository.
+ *
+ * @author Barbara Grabowska
+ * @version %I%, %G%
+ */
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+    /**
+     * @param name -  username of wanted user
+     * @return User object that has a given username or else null
+     */
+    User findByUsername(String name);
+}
