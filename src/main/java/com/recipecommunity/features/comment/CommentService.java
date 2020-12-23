a7a20a4c4cd4e5834cd299d470a5d1bd112242c8
BG678 <57709951+BG678@users.noreply.github.com>
2020-12-23 19:55:42 +0100

    Created comment service class

---
+++ src/main/java/com/recipecommunity/features/comment/CommentService.java
@@ -0,0 +1,69 @@
+package com.recipecommunity.features.comment;
+
+import com.recipecommunity.utils.ResourceNotFoundException;
+import com.recipecommunity.utils.UserByUsername;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service class for Comment feature.
+ *
+ * @author Barbara Grabowska
+ * @version %I%, %G%
+ */
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository repository;
+    @Autowired
+    private UserByUsername userByUsername;
+    private final Logger LOGGER = LoggerFactory.getLogger(CommentService.class);
+
+    /**
+     * Deletes Comment object only if the value of user's username of the argument is the same as current principal's username
+     *
+     * @param comment Comment object to be deleted
+     */
+    @PreAuthorize("#comment.user.username == authentication.principal.username")
+    protected void delete(Comment comment) {
+        LOGGER.debug("Deleting savedRecipe");
+        repository.delete(comment);
+    }
+
+    /**
+     * @param commentId id of wanted Comment object
+     * @param recipeId  id of a recipe that wanted comment belong to
+     * @return proper Comment object, if it does't exist this method throws new ResourceNotFoundException
+     */
+    protected Comment getOneById(Long commentId, Long recipeId) {
+        Comment comment = repository.findById(commentId).orElseThrow(ResourceNotFoundException::new);
+        if (comment.getRecipe().getId().equals(recipeId)) {
+            return comment;
+        }
+        throw new ResourceNotFoundException();
+    }
+
+    /**
+     * @param recipeId id of a recipe that wanted comments belong to
+     * @param pageable PageRequest object that should contain data about wanted page number and size
+     * @return wanted Page of a given recipe's comments
+     */
+    protected Page<Comment> findCommentsByRecipeId(Long recipeId, Pageable pageable) {
+        return repository.findByRecipeId(recipeId, pageable);
+    }
+
+    /**
+     * @param comment Comment object to be saved
+     * @return Comment object that just has been saved
+     */
+    protected Comment saveComment(Comment comment) {
+        return repository.save(comment);
+    }
+
+
+}
