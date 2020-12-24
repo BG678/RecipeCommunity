05139103e8acacdb98448c92e87e15b621cccdff
BG678 <57709951+BG678@users.noreply.github.com>
2020-12-25 00:26:44 +0100

    Created repository for recipe feature

---
+++ src/main/java/com/recipecommunity/features/recipe/RecipeRepository.java
@@ -0,0 +1,19 @@
+package com.recipecommunity.features.recipe;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+/**
+ * SavedRecipe Repository interface, extends Paging and Sorting Repository.
+ *
+ * @author Barbara Grabowska
+ * @version %I%, %G%
+ */
+@Repository
+public interface RecipeRepository extends PagingAndSortingRepository<Recipe, Long> {
+    Page<Recipe> findByAuthorUsername(String username, Pageable pageable);
+    List<Recipe> findAll();
+}
