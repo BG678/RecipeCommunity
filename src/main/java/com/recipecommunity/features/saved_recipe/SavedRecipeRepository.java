43709290c86f19cef3059db7753e89ac6fbfef66
BG678 <57709951+BG678@users.noreply.github.com>
2020-12-25 00:28:46 +0100

    made saved recipe repository extend SavedRecipeByUsernameAndRecipeId interface

--- src/main/java/com/recipecommunity/features/saved_recipe/SavedRecipeRepository.java
+++ src/main/java/com/recipecommunity/features/saved_recipe/SavedRecipeRepository.java
@@ -1,17 +1,20 @@
 package com.recipecommunity.features.saved_recipe;
 
+import com.recipecommunity.utils.SavedRecipeByUsernameAndRecipeId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * SavedRecipe Repository interface, extends Paging and Sorting Repository.
+ * SavedRecipe Repository interface, extends Paging and Sorting Repository and SavedRecipeByUsernameAndRecipeId.
  *
  * @author Barbara Grabowska
  * @version %I%, %G%
  */
 @Repository
-public interface SavedRecipeRepository extends PagingAndSortingRepository<SavedRecipe, Long> {
+public interface SavedRecipeRepository extends PagingAndSortingRepository<SavedRecipe, Long>, SavedRecipeByUsernameAndRecipeId {
     Page<SavedRecipe> findByUserUsername(String username, Pageable pageable);
+    @Override
+    SavedRecipe findByUserUsernameAndRecipeId(String username, Long id);
 }
