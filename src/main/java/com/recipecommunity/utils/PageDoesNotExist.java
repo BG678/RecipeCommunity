b91d97bb99f33f82da94b30273c181ea61bf914b
BG678 <57709951+BG678@users.noreply.github.com>
2020-12-27 00:54:34 +0100

    Implemented proper methods

--- src/main/java/com/recipecommunity/utils/PageDoesNotExist.java
+++ src/main/java/com/recipecommunity/utils/PageDoesNotExist.java
@@ -1,4 +1,23 @@
 package com.recipecommunity.utils;
 
-public class PageDoesNotExist extends RuntimeException{
+public class PageDoesNotExist extends RuntimeException {
+    private String message;
+
+    public PageDoesNotExist() {
+        message = "This page doesn't exist";
+    }
+
+    public PageDoesNotExist(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
 }
--- src/main/java/com/recipecommunity/utils/ResourceNotFoundException.java
+++ src/main/java/com/recipecommunity/utils/ResourceNotFoundException.java
@@ -2,6 +2,24 @@ package com.recipecommunity.utils;
 
 
 public class ResourceNotFoundException extends RuntimeException {
+    private String message;
+
+    public ResourceNotFoundException() {
+        message = "Requested resource could not be found";
+    }
+
+    public ResourceNotFoundException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
 
 }
